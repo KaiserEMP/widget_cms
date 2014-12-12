8ee5400d42f85fb9b6a5d3e6e8f52b64bae20c5b
KaiserEMP <kaidogta3@hotmail.com>
2014-12-12 20:49:13 +0200

    Fixed #2 - Rakenduse käivitamiseks peab URL-i sisestama faili nime.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WIDGET</title>
+</head>
+<body>
+
+<a href="crudexample/databases-read.php">LOE</a>
+<hr>
+<a href="crudexample/databases_create.php">LOO</a>
+<hr>
+<a href="crudexample/databases_update.php">MUUDA</a>
+<hr>
+<a href="crudexample/databases_delete.php">KUSTUTA</a>
+
+
+
+
+
+</body>
+</html>
